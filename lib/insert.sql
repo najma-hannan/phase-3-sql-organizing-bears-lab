d2e5d33383027f2a22530daaa46224ad4fa9abd8
Hannan <najma.mahat@student.moringaschool.com>
2023-04-02 11:10:44 +0300

    Completed lab on populating the bears and querying the bears tables

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 12, "M", "dark brown", "Calm", 1),
+("Rowdy", 4, "F", "White", "Anxious", 0),
+("Tabitha", 10, "F", "White", "Goofy", 0),
+("Sergeant Brown", 15, "M", "Brown", "Violent", 1),
+("Melissa", 20, "F", "Black", "Sociable", 1),
+("Grinch", 21, "M", "White", "Calm", 0),
+("Wendy", 12, "F", "Black", "Intense", 1),
+(NULL, NULL, "M", "Brown", "Aggressive", 0);
